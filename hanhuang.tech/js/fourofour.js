43a8165d5a37cf32bf5625123de49d9e7f3bf9cd
hanhuang <han@hanhuang.tech>
2022-12-18 23:35:56 +1300

    Add style to 404.html, remove redundant img files

---
+++ hanhuang.tech/js/fourofour.js
@@ -0,0 +1,48 @@
+const logoWrap = document.querySelector("div.logowrap");
+const logo = document.querySelector("div.logo");
+const habbitImg = document.querySelector("div.habbit-img");
+const habbitA = document.querySelector("img.habbit-a");
+const habbitB = document.querySelector("img.habbit-b");
+const bannerImg = document.querySelector("img.banner");
+const lantern = document.querySelector("#lantern");
+const habbitCry = new Audio("../hanhuang.tech/habbit-cry.mp3");
+const redHabbit = document.querySelector("div.four-flex img.red");
+const returnMe = document.querySelector("a.four-return");
+
+// habbit click
+habbitImg.addEventListener("click", function () {
+  habbitCry.play();
+
+  habbitA.classList.remove("animate");
+  void habbitA.offsetWidth;
+  habbitA.classList.add("animate");
+
+  habbitB.classList.remove("animate");
+  void habbitB.offsetWidth;
+  habbitB.classList.add("animate");
+
+  bannerImg.classList.remove("animate");
+  void bannerImg.offsetWidth;
+  bannerImg.classList.add("animate");
+
+  lantern.classList.remove("animate");
+  void lantern.offsetWidth;
+  lantern.classList.add("animate");
+}, false);
+
+// habbit change
+window.onmousemove = function (e) {
+  habbitChange(e);
+}
+
+function habbitChange(e) {
+  // clientX property returns the horizontal coordinate of the current window
+  let x = e.clientX;
+  let w = Math.floor(document.documentElement.clientWidth / 2);
+  if (x > w) { habbitB.style.opacity = "1"; }
+  else { habbitB.style.opacity = "0"; }
+}
+
+redHabbit.style.transitionDuration = "1s";
+returnMe.addEventListener("mouseenter", function () { redHabbit.style.opacity = "0"; });
+returnMe.addEventListener("mouseleave", function () { redHabbit.style.opacity = "1"; });
