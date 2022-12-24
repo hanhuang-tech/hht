4e588710067745008b88830fdccd375bb275c896
hanhuang <han@hanhuang.tech>
2022-12-24 17:13:55 +1300

    Add new style to contact.php

--- hanhuang.tech/contact/contact.php
+++ hanhuang.tech/contact/contact.php
@@ -3,12 +3,10 @@ $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
 $team = $_POST['team'];
-$to = "han@hanhuang.tech";
-
 $txt ="Name = ". $name . "\r\n Message =" . $email . $message . $team . "\r\n Email = " . ;
- 
-// $headers = "From: noreply@demosite.com" . "\r\n" .
-//             "CC: somebodyelse@example.com";
+
+$to = "han@hanhuang.tech"; 
+
 if ($email != null) {
     mail($to, $txt);
 }
