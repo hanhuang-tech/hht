65773662af89579842bc76506fb1c5b08f6ef298
hanhuang <han@hanhuang.tech>
2023-01-03 15:06:22 +1300

    Add new content in contact>index.php

--- hanhuang.tech/contact/index.php
+++ hanhuang.tech/contact/index.php
@@ -1,12 +1,28 @@
 <?php
+$response = [];
+// body
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $team = $_POST['team'];
-$formcontent ="From: $name \n Team: $team \n Message: $message";
-$recipient = "han@hanhuang.tech";
-$subject = "Message from contact form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+$body ="From: $name \n Team: $team \n Message: $message";
+
+// header
+$recipient = "noreply@hanhuang.tech";
+$header = "Recipient: $recipient \r\n From: $email \r\n";
+
+// conditions
+if (isset($email, $name, $message)) {
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $response[] = 'Please enter a valid email address';
+    }
+    if (empty($name) || empty($email) || empty($message)) {
+		$response[] = 'Please complete all fields';
+	} 
+    if (mail($header, $body)) {
+        $response[] = 'Message sent!';		
+    } else {
+        $response[] = 'Message could not be sent! Something seems to be wrong :-(';
+    }
+}
 ?>
