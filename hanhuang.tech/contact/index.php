8cc8bfe478b3acaafe315cd0dc32a83ea62b3f0b
hanhuang <han@hanhuang.tech>
2023-01-12 23:20:26 +1300

    Add new style of php

--- hanhuang.tech/contact/index.php
+++ hanhuang.tech/contact/index.php
@@ -1,54 +1,36 @@
 <?php
-// message to user
-$postData = $statusMsg = $statusErr = '';
-$status = 'error';
-// body
-$to = $_POST['email'];
-$subject = $_POST['subject'];
-$name = $_POST['name'];
-$team = $_POST['team'];
-$message = $_POST['message'];
-
-
-if(isset($_POST['submit'])){
-    $postData = $_POST;
-
-    if(empty($name)){
-         $statusErr .= 'Please enter your name.<br/>';
-    }
-    if(empty($email) || filter_var($email, FILTER_VALIDATE_EMAIL) === false){
-        $statusErr .= 'Please enter a valid email.<br/>';
-    }
-    if(empty($subject)){
-        $statusErr .= 'Please enter subject.<br/>';
-    }
-    if(empty($message)){
-        $statusErr .= 'Please enter your message.<br/>';
-    }
-    if(empty($statusErr)){
-        // Send email notification to the site admin
-        $subject = 'New contact request submitted';
-        $htmlContent = "
-            <h2>Contact Request Details</h2>
-            <p><b>Name: </b>".$name."</p>
-            <p><b>Email: </b>".$email."</p>
-            <p><b>Subject: </b>".$subject."</p>
-            <p><b>Team: </b>".$team."</p>
-            <p><b>Message: </b>".$message."</p>
-        ";
-        // Always set content-type when sending HTML email
-        $headers = "MIME-Version: 1.0" . "\r\n";
-        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-        // Header for sender info
-        $headers .= 'From:' . $name . ' <' . $email . '>' . "\r\n";
-        // Send email
-        @mail($email, $subject, $htmlContent, $headers);
-        $status = 'success';
-        $statusMsg = 'Thank you! Your contact request has submitted successfully, we will get back to you soon.';
-        $postData = '';
-    }else{
-        $statusMsg = '<p>Please fill all the mandatory fields:</p>' . trim($statusErr, '<br/>');
-    }
+// Output messages
+$responses = [];
+// Check if the form was submitted
+if (isset($_POST['email'], $_POST['subject'], $_POST['name'], $_POST['message'])) {
+	// Validate email adress
+	if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+		$responses[] = 'Email is not valid!';
+	}
+	// Make sure the form fields are not empty
+	if (empty($_POST['email']) || empty($_POST['subject']) || empty($_POST['name']) || empty($_POST['message'])) {
+		$responses[] = 'Please complete all fields!';
+	} 
+	// If there are no errors
+	if (!$responses) {
+		// Where to send the mail? It should be your email address
+		$to      = 'han@hanhuang.tech';
+		// Send mail from which email address?
+		$from = 'noreply@hanhuang.tech';
+		// Mail subject
+		$subject = $_POST['subject'];
+		// Mail message
+		$message = $_POST['message'];
+		// Mail headers
+		$headers = 'From: ' . $from . "\r\n" . 'Reply-To: ' . $_POST['email'] . "\r\n" . 'X-Mailer: PHP/' . phpversion();
+		// Try to send the mail
+		if (mail($to, $subject, $message, $headers)) {
+			// Success
+			$responses[] = 'Message sent!';		
+		} else {
+			// Fail
+			$responses[] = 'Message could not be sent! Please check your mail server settings!';
+		}
+	}
 }
-
 ?>
